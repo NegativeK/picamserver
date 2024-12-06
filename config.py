3b9aef03f730cc0834cb9275761e2d459dfc59ad
negk <git@negk.net>
2024-12-06 17:55:16 +0000

    Remove duplicate config

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import pathlib
 
 # TODO: These should be absolute (?) and configured by the user.
 IMAGE_FILE = pathlib.Path("./data/photo.jpg")
-IMAGE_FILE = pathlib.Path("./data/photo.jpg")
 LISTENER_PATH = pathlib.Path("./listeners")
 REFRESH_INTERVAL = 2
